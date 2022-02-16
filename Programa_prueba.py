7a60732cdbc550c57fc46fd02f84743e962c100d
Simon3640 <simon.garcial@udea.edu.co>
2022-02-16 15:13:42 -0500

    Comentando todas las lineas

--- Programa_prueba.py
+++ Programa_prueba.py
@@ -1,11 +1,11 @@
 #programa que determina si un numero es par
-N=int(input("Ingresar el numero : "))
+N=int(input("Ingresar el numero : ")) #ingresamos el numero
 
-if N%2 == 0:
-    print('N es par')
-else:
-    print('N es impar')
+if N%2 == 0: # vemos que el residuo de N entre 2 es 0
+    print('N es par') #Presentamos la informacion  
+else: #En caso de que no se cumpla la proposicion
+    print('N es impar') # Damos la informacion cuando no se cumple el condicional
 
-print('nuevo cambio')
+print('nuevo cambio') #cambio
 
-import numpy as np
+import numpy as np #importamos la libreria numpy
